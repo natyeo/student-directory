78b0f5b409f66d8d55edb36a857a96b5e119fa4d
natyeo <49326857+natyeo@users.noreply.github.com>
2019-10-07 19:05:00 +0100

    Q6. Center output

--- directory2.rb
+++ directory2.rb
@@ -27,21 +27,21 @@ def input_students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(100)
+  puts "-------------".center(100)
 end
 
 def print(students)
   index = 0
   while index < students.count do 
     student = students[index]
-    puts "#{index + 1}. #{student[:name]}, #{student[:dob]}, #{student[:country]} (#{student[:cohort]} cohort)"  
+    puts "#{index + 1}. #{student[:name]}, #{student[:dob]}, #{student[:country]} (#{student[:cohort]} cohort)".center(100)  
   index += 1
   end 
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{students.count} great students".center(100)
 end
 
 students = input_students
