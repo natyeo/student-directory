609bf6c90fd1322cf54918ffd8858d9838d8d280
natyeo <49326857+natyeo@users.noreply.github.com>
2019-10-07 21:07:34 +0100

    Q8. Group students by cohort

--- directory2.rb
+++ directory2.rb
@@ -16,10 +16,10 @@ def input_students
   end 
   # while the name is not empty, repeat this code
   while !name.empty? do
-  # add the student hash to the array
+    # add the student hash to the array
     students << {name: name, cohort: cohort, dob: dob, country: country} 
     puts "Now we have #{students.count} students"
-  # get another name and their personal info
+    # get another name and their personal info
     puts "Please enter another name"  
     name = gets.chomp
     puts "Please enter date of birth"
@@ -34,17 +34,20 @@ def input_students
 end
 
 def print_header
-  puts "The students of Villains Academy".center(100)
+  puts "The students of Villains Academy by cohort".center(100)
   puts "-------------".center(100)
 end
 
 def print(students)
-  index = 0
-  while index < students.count do 
-    student = students[index]
-    puts "#{index + 1}. #{student[:name]}, #{student[:dob]}, #{student[:country]} (#{student[:cohort]} cohort)".center(100)  
-  index += 1
-  end 
+  group_by_cohort = {}
+  students.each { |student| 
+    c = student[:cohort]
+    if group_by_cohort[c] == nil
+      group_by_cohort[c] = []
+    end
+    group_by_cohort[c].push(student[:name])
+  }
+  puts group_by_cohort.to_a
 end
 
 def print_footer(students)
