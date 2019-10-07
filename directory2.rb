fe28b8f1b3450031c966c38d7a7baebc11ee72f4
natyeo <49326857+natyeo@users.noreply.github.com>
2019-10-07 18:47:28 +0100

    Q5. Get more info about students

--- directory2.rb
+++ directory2.rb
@@ -3,15 +3,24 @@ def input_students
   puts "To finish, just hit return twice"
   # create an empty array
   students = []
-  # get the first name
+  # get the first name and their personal info from the user
   name = gets.chomp
+  puts "Please enter their date of birth"
+  dob = gets.chomp
+  puts "Please enter their country of birth"
+  country = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
   # add the student hash to the array
-    students << {name: name, cohort: :November}
+    students << {name: name, cohort: :November, dob: dob, country: country}
     puts "Now we have #{students.count} students"
-  # get another name from the user
+  # get another name and their personal info
+    puts "Please enter another name"  
     name = gets.chomp
+    puts "Please enter date of birth"
+    dob = gets.chomp
+    puts "Please enter country of birth"
+    country = gets.chomp
   end
   # return the array of input_students
   students
@@ -25,7 +34,8 @@ end
 def print(students)
   index = 0
   while index < students.count do 
-    puts "#{index + 1}. #{students[index][:name]} (#{students[index][:cohort]} cohort)"  
+    student = students[index]
+    puts "#{index + 1}. #{student[:name]}, #{student[:dob]}, #{student[:country]} (#{student[:cohort]} cohort)"  
   index += 1
   end 
 end
