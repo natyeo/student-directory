e9ac5e71db3298fa018341c9e15435c06ae9f32c
natyeo <49326857+natyeo@users.noreply.github.com>
2019-10-07 15:49:14 +0100

    Q3. Print students whose name is shorter than 12 characters

--- directory2.rb
+++ directory2.rb
@@ -24,7 +24,7 @@ end
 
 def print(students)
   students.each.with_index(1) do |student, index|
-    if student[:name].start_with?('H')
+    if student[:name].length < 12
     puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
     end 
   end
